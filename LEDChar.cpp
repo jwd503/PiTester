467777179a323f8f1cc6a998dd47ff6c3241f0e3
jwd503 <jwd503@york.ac.uk>
2017-04-18 16:09:46 +0000

    fixed an issue with the segmentSelect mapping indexing from 1, introduced some functions to enable checking if a particular segment should be activated given a bitmap

--- LEDChar.cpp
+++ LEDChar.cpp
@@ -1,6 +1,7 @@
 #include "LEDChar.h"
+#include "PJ_RPI.h"
 
-LEDChar::LEDChar(int index){
+LEDChar::LEDChar(int index, int segments){
 	switch(index){
 		case 0:
 			ledSelect = 1<<9;
@@ -14,13 +15,14 @@ LEDChar::LEDChar(int index){
 		case 3:
 			ledSelect = 1<<26;
 			break;
-		case default:
+		default:
 			ledSelect = 0;
 			break;
 	}
+	this->segments = segments;
 }
 
-LEDChar::setSegmentSelect(int segment){
+void LEDChar::setSegmentSelect(int segment){
 	//0 GPIO26
 	//1 GPIO26 + GPIO9
 	//2 GPIO26 + GPIO20
@@ -29,37 +31,70 @@ LEDChar::setSegmentSelect(int segment){
 	//5 GPIO26 + GPIO23 + GPIO9
 	//6 GPIO26 + GPIO23 + GPIO20
 	//7 GPIO26 + GPI23 + GPIO20 + GPIO9
-	int gpio9 = 1 << 9;
-	int gpio20 = 1 << 20;
-	int gpio23 = 1 << 23;
 
 	segmentSelect = 1 << 26;
 
 	switch(segment){
-		case 1:	//A Y0 Top
+		case 0:	//A Y0 Top
 			//segmentSelect = 0;
 			break;
-		case 2:	//B Y1 Top right
+		case 1:	//B Y1 Top right
 			segmentSelect |= gpio9;
 			break;
-		case 3:	//C Y2 Bottom right
+		case 2:	//C Y2 Bottom right
 			segmentSelect |= gpio20;
 			break;
-		case 4:	//D Y3 Bottom
+		case 3:	//D Y3 Bottom
 			segmentSelect |= gpio9 | gpio20;
 			break;
-		case 5:	//E Y4 Botom left
+		case 4:	//E Y4 Botom left
 			segmentSelect |= gpio23;
 			break;
-		case 6:	//F Y5 Top Left
+		case 5:	//F Y5 Top Left
 			segmentSelect |= gpio23 | gpio9;
 			break;
-		case 7:	//G Y6 Middle
+		case 6:	//G Y6 Middle
 			segmentSelect |= gpio23 | gpio20;;
 			break;
-		case 8:	//DP Y7 DP
+		case 7:	//DP Y7 DP
 			segmentSelect |= gpio23 | gpio20 | gpio9;
 			break;
 	}
 }
 
+int LEDChar::getLEDPins(){
+	return gpio9 | gpio23 | gpio20 | (1<<26);
+}
+
+void LEDChar::clearRegister(){
+	INP_GPIO(9);
+	OUT_GPIO(9);
+	INP_GPIO(18);
+	OUT_GPIO(18);
+	INP_GPIO(20);
+	OUT_GPIO(20);
+	INP_GPIO(23);
+	OUT_GPIO(23);
+	INP_GPIO(26);
+	OUT_GPIO(26);
+
+	GPIO_CLR = getLEDPins();
+	GPIO_SET = 1<<18;
+	GPIO_CLR = 1<<18;
+	GPIO_SET = 1<<18;
+	GPIO_CLR = 1<<18;
+}
+
+int LEDChar::decode(int bit){
+	//segments 0-127 1 bit per segment
+	if (segments & (1<<bit)){
+		setSegmentSelect(bit);
+		return segmentSelect;
+	}
+	segmentSelect = 0;
+	return 0;
+}
+
+void LEDChar::setSegments(int segments){
+	this->segments = segments;
+}
--- LEDChar.h
+++ LEDChar.h
@@ -1,10 +1,24 @@
+#ifndef LEDCHAR_H
+#define LEDCHAR_H
+#include <string.h>
+#include <map>
 class LEDChar{
 	private:
 		int ledSelect;
+		static const int gpio9 = 1 << 9;
+        	static const int gpio20 = 1 << 20;
+	        static const int gpio23 = 1 << 23;
+		static std::map<std::string, int> charSet;
 		int segmentSelect;
 
 	public:
-		LEDChar();
+		LEDChar(int index, int segments);
 		void setSegmentSelect(int segment);
+		void setSegments(int segments);
+		int getLEDPins();
+		void clearRegister();
+		int decode(int bit);
+		int segments;
 
 };
+#endif
