0e5d6b2e582e3aa1051aed4a29b65082186c7feb
jwd503 <jwd503@york.ac.uk>
2017-04-25 09:24:19 +0000

    Introduced a way to output text to the 7 segment display

--- LEDDriver.cpp
+++ LEDDriver.cpp
@@ -2,6 +2,41 @@
 #include "LEDChar.h"
 #include "string.h"
 #include <map>
+
+const uint8_t LEDDriver::sevsegascii_table[] = {
+	SEGN, SEGN, SEGN, SEGN,
+	SEGN, SEGN, SEGN, SEGN,
+	SEGN, SEGN, SEGN, SEGN,
+	SEGN, SEGN, SEGN, SEGN,
+	SEGN, SEGN, SEGN, SEGN,
+	SEGN, SEGN, SEGN, SEGN,
+	SEGN, SEGN, SEGN, SEGN,
+	SEGN, SEGN, SEGN, SEGN,
+	SEGN, SEGN, SEGN, SEGN,
+	SEGN, SEGN, SEGN, SEGN,
+	SEGN, SEGN, SEGN, SEGN,
+	SEGN, SEGN, 0x80, SEGN,
+	0x3F, 0x06, 0x5B, 0x4F, // 0 1 2 3
+	0x66, 0x6D, 0x7D, 0x07, // 4 5 6 7
+	0x7F, 0x6F, SEGN, SEGN, // 8 9
+	SEGN, SEGN, SEGN, SEGN,
+	SEGN, 0x77, 0x7C, 0x39, //   A B C
+	0x5E, 0x79, 0x71, 0x3D, // D E F G
+	0x74, 0x30, 0x1E, SEGN, // H I J _
+	0x38, 0x37, 0x54, 0x3F, // L M N O
+	0x73, 0x67, 0x50, 0x6D, // P Q R S
+	0x78, 0x3E, 0x1C, SEGN, // T U V _
+	SEGN, 0x6E, 0x5B, SEGN, // _ Y Z
+	SEGN, SEGN, SEGN, SEGN,
+	SEGN, 0x77, 0x7C, 0x39, //   A B C
+	0x5E, 0x79, 0x71, 0x3D, // D E F G
+	0x74, 0x30, 0x1E, SEGN, // H I J _
+	0x38, 0x37, 0x54, 0x3F, // L M N O
+	0x73, 0x67, 0x50, 0x6D, // P Q R S
+	0x78, 0x3E, 0x1C, SEGN, // T U V _
+	SEGN, 0x6E, 0x5B, SEGN, // _ Y Z
+	SEGN, SEGN, SEGN, SEGN
+};
 std::map<std::string, int> LEDDriver::charSet = {
         {"0", 0x3F},
         {"1", 0x06},
@@ -32,16 +67,72 @@ void LEDDriver::decodeWord(std::string word){
 	//This only checks the first 4 chars of the string parsed as there are only 4 LED displays
 	for(int charIndex = 0; charIndex < 4; charIndex++){
 		//Fetch the char and cast as a string
-		std::string s(1,word[charIndex]);
+//		std::string s(1,word[charIndex]);
+
+		LEDChars[charIndex].setSegments(sevsegascii_table[word[charIndex]]);
 		//if the following condition is met the key was found in the mapping
-		if(charSet.find(s) != charSet.end()){
-			printf("word[%d]: %x\n", charIndex, charSet.at(s));
+//		if(charSet.find(s) != charSet.end()){
+//			printf("word[%d]: %x\n", charIndex, charSet.at(s));
 			//Set the LEDs segments to display to the value from the mapping
-			LEDChars[charIndex].setSegments(charSet.at(s));
-		}else{
+//			LEDChars[charIndex].setSegments(charSet.at(s));
+//		}else{
 			//Couldn't find a mapping for this character so set blank instead
-			LEDChars[charIndex].setSegments(0);
-		}
+//			LEDChars[charIndex].setSegments(0);
+//		}
 
 	}
+}
+
+void LEDDriver::setDisplay(){
+	for(int segmentIndex = 0; segmentIndex < 8; segmentIndex++){
+		int ledSelect = 0;
+		int segmentSelected = 0;
+		LEDChars[0].setSegmentSelect(segmentIndex);
+		segmentSelected = LEDChars[0].segmentSelect;
+		for (int ledIndex = 0; ledIndex < 4; ledIndex++){
+			int segmentSelectInd = ((LEDChars[ledIndex].segments & (1<<segmentIndex)) > 0);
+			if (segmentSelectInd != 0){
+				ledSelect |= LEDChars[ledIndex].ledSelect;
+			}
+		}
+		int delay = 10;
+		GPIO_SET = segmentSelected;
+		delayMicroseconds(delay);
+		GPIO_SET = 1<<18;
+		GPIO_CLR = 1<<18;
+		GPIO_CLR = segmentSelected;
+
+		GPIO_SET = ledSelect;
+		delayMicroseconds(delay);
+		GPIO_SET = 1<<18;
+		GPIO_CLR = 1<<18;
+		GPIO_CLR = ledSelect;
+		delayMicroseconds(1000);
+	}
+
+}
+
+void LEDDriver::driveDisplay(std::string word){
+	float duration = 0.3;
+	int iterations = 125 * duration;
+	decodeWord(word);
+	setOutputs((1<<26) | (1<<23) | (1<<6) | (1<<9) |(1<<18));
+	LEDChars[0].clearRegister();
+	for(int i = 0; i < iterations; i++){
+		setDisplay();
+	}
+	LEDChars[0].clearRegister();
+
+}
+
+void LEDDriver::driveDisplay(std::string word, float duration){
+	int iterations = 125 * duration;
+	decodeWord(word);
+	setOutputs((1<<26) | (1<<23) | (1<<6) | (1<<9) |(1<<18));
+	LEDChars[0].clearRegister();
+	for(int i = 0; i < iterations; i++){
+		setDisplay();
+	}
+	LEDChars[0].clearRegister();
+
 }
--- LEDDriver.h
+++ LEDDriver.h
@@ -2,7 +2,8 @@
 #include "string.h"
 #include <map>
 #include "LEDChar.h"
-
+#include "PJ_RPI.h"
+#include "Utility.h"
 class LEDDriver{
 	//Uses GPIO 26,23,29,9 to write data
 	//GPIO 18 to clock data in
@@ -11,9 +12,15 @@ class LEDDriver{
 	private:
 		std::vector<LEDChar> LEDChars;
 		static std::map<std::string,int> charSet;
+		#define SEGN 0x00
+		#define SEGU 0x08 //unknown way of display this character
+		static const uint8_t sevsegascii_table[];
+		void setDisplay();
+
 	public:
 		LEDDriver();
-		void driveDisplay();
+		void driveDisplay(std::string word);
+		void driveDisplay(std::string word, float duration);
 		void decodeWord(std::string word);
 
 };
