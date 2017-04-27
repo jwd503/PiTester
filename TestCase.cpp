c381a98304162240aadce79e58dcdcfb8f84ea86
jwd503 <jwd503@york.ac.uk>
2017-04-27 15:54:31 +0000

    Passed the errorPointer to TestCase such that it can generate the new style errors

--- TestCase.cpp
+++ TestCase.cpp
@@ -1,9 +1,10 @@
 #include "TestCase.h"
 #include <stdio.h>
-TestCase::TestCase(int outputMask, int expectedResult):
+TestCase::TestCase(int outputMask, int expectedResult, ErrorReporting* errorPointer):
 	readResult{0}{
 	this->outputMask = outputMask;
 	this->expectedResult = expectedResult;
+	e = errorPointer;
 }
 
 
@@ -11,30 +12,51 @@ int TestCase::compareAll(){
 	int i = 0;
 	int result = 0;
 	//Skip the first result as signal propagation errors show up here
-	for(i = 8; i < NREADS-6; i++){
+	for(i = 8; i < NREADS-12; i++){
 		int tempResult = compareOne(i);
 		if(tempResult != 0){
-			printf("Read no:%d\n", i);
+//			printf("Read no:%d\n", i);
 		}
 		result |= tempResult;
 	}
 
-	return (result == 1)? 0: 1;
+	int pin = outputMask & ~(1<<6);
+	int pinIndex = 0;
+	for(pinIndex = 0; pinIndex< 32; pinIndex++){
+		if( (pin & (1<<pinIndex)) > 0){
+			break;
+		}
+	}
+
+	if((result & 1) > 0) { //Short circuit
+		int errorCode = e->generateErrorCode(STATIC_TEST, PIN_LEVEL, pinIndex, ELECTRICAL_SHORT);
+		e->setNextErrorCode(errorCode, 100.0);
+	}
+
+	if((result & 2) > 0) { //Open circuit
+		int errorCode = e->generateErrorCode(STATIC_TEST, PIN_LEVEL, pinIndex, OPEN_CIRCUIT); //Change to OC code
+		e->setNextErrorCode(errorCode, 100.0);
+	}
+
+	return result;
 }
 
 int TestCase::compareOne(int id){
-	if(id < NREADS){
-//		printf("observed: %d, expected: %d\n", (readResult[id] & ~(1 << 6)) ,(expectedResult  & ~(1 << 6)));
-		int mask = ~(1<<6);// & ~(1<<2) & ~(1<<3);
-		if ((readResult[id] & mask) == (expectedResult  & mask)){
-			return 0;
-		} else{
-			printf("observed: %d, expected: %d\n", (readResult[id] & ~(1 << 6)) ,(expectedResult  & ~(1 << 6)));
-
-			return 1;
-		}
-	} else{
+	if(id >= NREADS){ //invalid id provided
 		return 1;
+	}
+
+	int mask = ~(1<<6);
+	//Result ok
+	if ((readResult[id] & mask) == (expectedResult  & mask)){
+		return 0;
+	} else{
+//		printf("observed: %d, expected: %d\n", (readResult[id] & ~(1 << 6)) ,(expectedResult  & ~(1 << 6)));
+		if(readResult[id] == outputMask){
+			return 2;//Open circuit
+		}else{
+			return 1;//Short circuit
+		}
 	}
 }
 
--- TestCase.h
+++ TestCase.h
@@ -3,14 +3,15 @@
 #ifndef NREADS
 	#define NREADS 32
 #endif
+#include "ErrorReporting.h"
 class TestCase{
 	private:
 		int expectedResult;
 		int outputMask;
 	public:
 		int readResult[NREADS];
-
-		TestCase(int outputMask, int expectedResult);
+		ErrorReporting* e;
+		TestCase(int outputMask, int expectedResult, ErrorReporting* e);
 		void setReads(int nReads);
 		int compareAll();
 		int compareOne(int id);
